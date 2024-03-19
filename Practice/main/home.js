95e015a7901797a9da5545cb40ecf57163c93e51
SeongYun_Cheon <1112csy@naver.com>
2024-03-19 14:32:16 +0900

    0319스터디 전 커밋

---
+++ Practice/main/home.js
@@ -0,0 +1,15 @@
+// 스크롤바
+let scrollTop = 0;
+let bar = document.getElementsByClassName("bar-ing")[0];
+
+window.addEventListener(
+  "scroll",
+  () => {
+    scrollTop = document.documentElement.scrollTop;
+    let per = Math.ceil(
+      (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+    );
+    bar.style.width = per + "%";
+  },
+  false
+);
